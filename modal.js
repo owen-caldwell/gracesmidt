7a9728a58deb437a0d69b68f9a04ea5fc067452c
owen-caldwell <caldwellom@gmail.com>
2023-09-26 13:18:27 -0400

    latest notes

---
+++ modal.js
@@ -0,0 +1,6 @@
+const imggallery = document.querySelector(".gallery");
+const imgs = imggallery.children;
+const modal = document.querySelector(".modal");
+const modalBtn = document.querySelector(".modalBtn");
+const modalLeftBtn = document.querySelector("#modalLeft");
+const modalRightBtn = document.querySelector("#modalRight");
